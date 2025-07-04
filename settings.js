dec6f2e206494b02ea4a8a3783f0ff224862761d
Lucky Tech Hub <luckytechhub1@gmail.com>
2025-07-04 19:15:05 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Lucky Tech Hub Bot',
+  author: '‎',
+  botName: "Lucky Tech Hub Bot",
+  botOwner: 'Lucky218', // Your name
+  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
