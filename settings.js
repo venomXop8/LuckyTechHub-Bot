20425d81b20c7ec36612288cb743e5e5954a1b25
IRONMAN x VENOM <gautamdipanshu475@gmail.com>
2025-07-06 20:55:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Lucky Tech Hub Bot',
-  author: '‎',
-  botName: "Lucky Tech Hub Bot",
-  botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Cybro',
+  author: '‎kaminari',
+  botName: "Cybro",
+  botOwner: 'raj', // Your name
+  ownerNumber: '918292001437', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
